f4c97d20f024ef4859fdc337fcf7d30c54a0a08b
sakost <sakost01@gmail.com>
2020-04-19 03:45:27 +0300

    upgrade command `server`

--- bot/plugins/server.py
+++ bot/plugins/server.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 import psutil
@@ -10,10 +11,11 @@ from bot.roles import admin_role
 plugin = Plugin('Server[admin]', 'pings to servers')
 
 
-@plugin.on_commands(['server'])
+@plugin.on_commands(['метрик', 'server', 'серв', 'metrics'])
 @admin_role
 async def _(msg, ctx):
     p = psutil.cpu_percent(interval=1, percpu=True)
+    cur_process = psutil.Process(os.getpid())
     m = psutil.virtual_memory()
     answer = ''
     for num, process in enumerate(p):
@@ -27,7 +29,8 @@ async def _(msg, ctx):
         answer += f"Ping(vk api): {(await aioping.ping('api.vk.com')) * 1000:.2f} ms\n"
     except TimeoutError:
         answer += f"Ping(vk api): >10sec"
-    elapsed_time = int(time.time()) - msg.raw['object']['message']['date']
+    answer += f"Бот занимает: {cur_process.memory_info().rss / 2**20:.2f}MiB памяти\n"
+    elapsed_time = int(time.time()) - msg.date
     if elapsed_time > 0:
         answer += f'Прошло {elapsed_time} секунд с момента получения сообщения\n'
     await ctx.reply(answer)
