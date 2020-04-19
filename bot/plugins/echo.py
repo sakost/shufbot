dd7cb0faca53ad98f172d8487d7bbe59c274a96c
sakost <sakost01@gmail.com>
2020-04-19 20:07:53 +0300

    add command `ae` instead `echo`

--- bot/plugins/echo.py
+++ bot/plugins/echo.py
@@ -4,6 +4,6 @@ from kutana import Plugin, Context, Message
 plugin = Plugin(name='Echo', description='Напиши мне кто ты и я скажу кто ты')
 
 
-@plugin.on_commands(['эхо', 'echo'])
+@plugin.on_commands(['ае', 'ae'])
 async def _(msg: Message, ctx: Context):
-    await ctx.reply(ctx.body, attachments=msg.attachments)
+    await ctx.reply('Ае')
