ef8a18269013aa6aed694457af876a917ed9e915
sakost <sakost01@gmail.com>
2020-04-20 22:56:09 +0300

    maybe fix bug on set_role x2

--- bot/plugins/manage_chat_roles.py
+++ bot/plugins/manage_chat_roles.py
@@ -19,6 +19,7 @@ plugin = Plugin('Manage chat users[develop]', 'Manage chat users roles')
 @admin_role
 @chat_only
 async def _(msg, ctx):
+    level_name = None
     for lvl, lvl_names in CHAT_USER_ROLES.items():
         if lvl == ChatUserRoles.ADMIN and ctx.chat_user.role < ChatUserRoles.CREATOR.value and \
                 ctx.user.role < UserRoles.DEVELOPER:
@@ -26,14 +27,21 @@ async def _(msg, ctx):
         for name in lvl_names:
             if ctx.body.startswith(name):
                 users = await extract_users(msg, ctx)
+                if not users:
+                    continue
                 async with ctx.mgr.atomic():
                     for user_id in users:
                         user, _ = await ctx.mgr.get_or_create(User, id=user_id)
                         chat_user, _ = await ctx.mgr.get_or_create(ChatUser, user=user, chat=ctx.chat)
                         chat_user.role = lvl.value
                         await ctx.mgr.update(chat_user)
-                await ctx.reply(f'Пользователям установлен уровень доступа {name}')
-                return
+                    level_name = name
+    else:
+        if level_name is not None:
+            await ctx.reply(f'Пользователям установлен уровень доступа {level_name}')
+        else:
+            await ctx.reply('Не найдено ни одного пользователя')
+        return
     await ctx.reply('Неизвестный уровень доступа для пользователя. Доступные уровни: \n' + '\n'.join(
         f"{num}){', '.join(lvl_names)}" for num, lvl_names in
         enumerate(list(CHAT_USER_ROLES.values())[:-1-bool(ctx.chat_user.role != ChatUserRoles.CREATOR.value)], 1)
