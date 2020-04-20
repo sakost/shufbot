7ef918e8e5ab26bede0694b91d189e6fed662cf2
Daniel Glitch <winswin@protonmail.com>
2020-04-20 14:48:17 +0300

    removed лишние actions from check_admin and added saving admin state to db

--- bot/plugins/check_admin.py
+++ bot/plugins/check_admin.py
@@ -1,18 +1,22 @@
 from kutana import Plugin, Context
 from kutana.exceptions import RequestException
 
+from bot.db import Chat
+
 
 plugin = Plugin('Check admin rights', 'Проверяет, есть ли права Администратора у бота')
 
 
 @plugin.on_commands(['чекадм', 'check_access', 'check_admin'])
 async def _(msg, ctx: Context):
+    mgr = ctx.config['db_manager']
+    peer_id = msg.receiver_id
     try:
-        resp = await ctx.request('messages.getConversationMembers', peer_id=msg.receiver_id)
+        resp = await ctx.request('messages.getConversationMembers', peer_id=peer_id)
     except RequestException as e:
         await ctx.reply('Я не админ :с')
     else:
-        for member in resp['items']:
-            if member['member_id'] == -ctx.backend.group_id:
-                await ctx.reply('Я админ.. хе-хе' if member['is_admin'] else 'Я не админ :с')
-                break
+        await ctx.reply('Я админ.. хе-хе')
+        chat, created = await mgr.get_or_create(Chat, id=peer_id)
+        chat.admin = True
+        await mgr.update(chat)
