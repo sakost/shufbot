59fdc5e61713284dd2bca6bd522db68622bd22c1
sakost <sakost01@gmail.com>
2020-04-24 01:56:50 +0300

    refactor command that printing whenever user invited

--- bot/plugins/hello.py
+++ bot/plugins/hello.py
@@ -1,17 +1,37 @@
+import time
+
 from kutana import HandlerResponse
 
-from bot.roles import needed_admin_rights
+from bot.roles import needed_admin_rights, ChatUserRoles
 from bot.plugin import CustomPlugin
-from bot.router import AnyMessageRouterCustom
+from bot.db import User, ChatUser
+from bot.plugins.kick import kick_users
 
 plugin = CustomPlugin('Hello message')
 
 
-@plugin.on_router(AnyMessageRouterCustom)
+@plugin.on_message_action('chat_invite_user')
+@plugin.on_message_action('chat_invite_user_by_link')
 @needed_admin_rights
 async def _(msg, ctx):
     if not hasattr(ctx, 'chat'):
         return HandlerResponse.SKIPPED
-    if not msg.raw['object']['message'].get('action', {'type': ''})['type'].startswith('chat_invite_user'):
-        return HandlerResponse.SKIPPED
-    await ctx.reply('КУ!')
+    if ctx.action_type == 'chat_invite_user':
+        user_added, _ = await ctx.mgr.get_or_create(User, id=ctx.action['member_id'])
+    else:
+        user_added, _ = await ctx.mgr.get_or_create(User, id=msg.sender_id)
+    chat_user_added, _ = await ctx.mgr.get_or_create(ChatUser, user=user_added, chat=ctx.chat)
+    hello_message = 'КУ!\n'
+    # ctx.request("", ) # todo get a registration date
+    if not chat_user_added.banned:
+        await ctx.reply(hello_message)
+    if (chat_user_added.banned_until == -1 or chat_user_added.banned_until > time.time()) \
+            and ctx.chat_user.role < ChatUserRoles.ADMIN:
+        return await kick_users(ctx, [(user_added, chat_user_added)], msg.receiver_id - 2 * 10 ** 9)
+    else:
+        chat_user_added.banned = False
+        chat_user_added.banned_until = 0
+        await ctx.mgr.update(chat_user_added)
+        await ctx.reply(hello_message)
+        await ctx.reply(f'[id{user_added.id}|Пользователь] разбанен', disable_mentions=1)
+        return
