ce6c21eff4e7bd2e45986b3d56dad3f61be4c1b5
sakost <sakost01@gmail.com>
2020-04-24 03:50:02 +0300

    add handling of invitations

--- bot/plugins/hello.py
+++ bot/plugins/hello.py
@@ -7,8 +7,12 @@ from bot.plugin import CustomPlugin
 from bot.db import User, ChatUser
 from bot.plugins.kick import kick_users
 from bot.plugins.reg_date import get_registration_date, format_registration_date
+from bot.plugins.upd_users import update_users
 
 plugin = CustomPlugin('Hello message')
+
+hello_message = "Привет! Я Шаф(еш), и я бот-администратор бесед.\n" \
+                    "Помощь по боту - https://vk.com/@shufbot-help\n"
 
 
 @plugin.on_message_action('chat_invite_user')
@@ -17,19 +21,19 @@ plugin = CustomPlugin('Hello message')
 async def _(msg, ctx):
     if not hasattr(ctx, 'chat'):
         return HandlerResponse.SKIPPED
-    ctx.send_message(ctx.config['owner_id'], f"Action: {ctx.action!r}\nMessage:{msg.raw!r}")
-    print(f"Action: {ctx.action!r}\nMessage:{msg.raw!r}")
     if ctx.action_type == 'chat_invite_user':
+        if ctx.action['member_id'] == -int(ctx.backend.group_id):
+            await ctx.reply(hello_message)
+            await update_users(ctx)
+            return
         user_added, _ = await ctx.mgr.get_or_create(User, id=ctx.action['member_id'])
     else:
         user_added, _ = await ctx.mgr.get_or_create(User, id=msg.sender_id)
     chat_user_added, _ = await ctx.mgr.get_or_create(ChatUser, user=user_added, chat=ctx.chat)
     user_time, user_date = format_registration_date(await get_registration_date(ctx.backend.session, user_added.id))
-    hello_message = "Привет! Я Шаф(еш), и я бот-администратор бесед.\n" \
-                    "Помощь по боту - https://vk.com/@shufbot-help\n" \
-                    f"Твоя дата регистрации: {user_date}"
+    local_hello_message = hello_message + f"Твоя дата регистрации: {user_date}"
     if not chat_user_added.banned:
-        await ctx.reply(hello_message)
+        await ctx.reply(local_hello_message)
         return
     if (chat_user_added.banned_until == -1 or chat_user_added.banned_until > time.time()) \
             and ctx.chat_user.role < ChatUserRoles.ADMIN:
@@ -38,6 +42,6 @@ async def _(msg, ctx):
         chat_user_added.banned = False
         chat_user_added.banned_until = 0
         await ctx.mgr.update(chat_user_added)
-        await ctx.reply(hello_message)
+        await ctx.reply(local_hello_message)
         await ctx.reply(f'[id{user_added.id}|Пользователь] разбанен', disable_mentions=1)
         return
