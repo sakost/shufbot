73e2c252f0ef2f218ad59332b2db65873c8ef161
sakost <sakost01@gmail.com>
2020-04-24 04:12:07 +0300

    add restrictions to kick&ban commands & add command `kick`

--- bot/plugins/kick.py
+++ bot/plugins/kick.py
@@ -1,24 +1,37 @@
 from kutana import Plugin
 
-from bot.roles import admin_role, needed_admin_rights
-from bot.utils import extract_users
 from bot.db import ChatUser, User
+from bot.roles import admin_role, needed_admin_rights, chat_only
+from bot.utils import extract_users
 
 plugin = Plugin('Kick', 'кикает пользователя из беседы')
 
 
-@plugin.on_commands(['бан', 'ban', 'кик', 'kick'])
+@plugin.on_commands(['бан', 'ban'])
 @needed_admin_rights
 @admin_role
+@chat_only
 async def _(msg, ctx):
     users = await extract_users(msg, ctx)
     if users:
-        await kick_users(ctx, users, msg.receiver_id - 2*10**9)
+        await kick_users(ctx, users, msg.receiver_id - 2 * 10 ** 9)
     else:
         await ctx.reply('Вы не указали ни одного пользователя')
 
 
-async def kick_users(ctx, users, chat_id):
+@plugin.on_commands(['кик', 'kick'])
+@needed_admin_rights
+@admin_role
+@chat_only
+async def _(msg, ctx):
+    users = await extract_users(msg, ctx)
+    if users:
+        await kick_users(ctx, users, msg.receiver_id - 2 * 10 ** 9, ban=False)
+    else:
+        await ctx.reply('Вы не указали ни одного пользователя')
+
+
+async def kick_users(ctx, users, chat_id, ban=True):
     async with ctx.mgr.atomic():
         for user_id in users:
             if isinstance(user_id, tuple):
@@ -28,6 +41,7 @@ async def kick_users(ctx, users, chat_id):
                 user, _ = await ctx.mgr.get_or_create(User, id=user_id)
                 chat_user, _ = await ctx.mgr.get_or_create(ChatUser, user=user, chat=ctx.chat)
             await ctx.request('messages.removeChatUser', chat_id=chat_id, member_id=user.id)
-            chat_user.banned = True
-            chat_user.banned_until = -1
-            await ctx.mgr.update(chat_user)
+            if ban:
+                chat_user.banned = True
+                chat_user.banned_until = -1
+                await ctx.mgr.update(chat_user)
