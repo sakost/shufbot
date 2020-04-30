3c278c6b3ee8a5614251b642cb090b5ad67f3ce8
Daniel Glitch <winswin@protonmail.com>
2020-05-01 01:32:07 +0300

    fixedd reg_date and restart messgaes

--- bot/plugins/restart.py
+++ bot/plugins/restart.py
@@ -18,7 +18,7 @@ async def _(app):
                 elapsed_time = time.time() - float(sys.argv[2])
                 chat_id = int(sys.argv[3])
                 user_id = int(sys.argv[4])
-                user = (await get_users(ctx, user_id, 'ins'))[0]
+                user = (await get_users(backend, user_id, 'ins'))[0]
                 user_name = user['first_name'] + ' ' + user['last_name']
                 await backend.send_message(chat_id,
                                            f"Бот был перезапущен [id{user_id}|{user_name}] за {elapsed_time:.2f} сек",
