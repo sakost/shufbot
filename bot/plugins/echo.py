94f0994cb7ca1ff96b187fb4da61f6df59664b25
sakost <sakost01@gmail.com>
2020-05-10 13:11:52 +0300

    add some fun commands

--- bot/plugins/echo.py
+++ bot/plugins/echo.py
@@ -3,6 +3,6 @@ from kutana import Plugin, Context, Message
 plugin = Plugin(name='Аы', description='Напиши мне кто ты и я скажу кто ты')
 
 
-@plugin.on_commands(['аы', 'шуе', 'ыа', 'ку'])
+@plugin.on_commands(['аы', 'шуе', 'ыа', 'ку', 'ыы', 'шпш'])
 async def _(msg: Message, ctx: Context):
     await ctx.reply(ctx.command.capitalize())
