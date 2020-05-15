ae8c8bb21e5f8660926d35ac28799d599172cea3
sakost <sakost01@gmail.com>
2020-05-15 11:22:22 +0300

    maybe fix x3

--- bot/plugins/unknown_command.py
+++ bot/plugins/unknown_command.py
@@ -5,6 +5,6 @@ plugin = Plugin('Unknown command')
 
 @plugin.on_any_unprocessed_message()
 async def _(msg, ctx):
-    print('prosto pidor')
+    print('prosto pidor', msg.text)
     if ctx.with_prefix:
         await ctx.reply('Неизвестная команда')
